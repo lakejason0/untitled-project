8b797d03fda0625d38cbe1ef2482a8802832c42f
lakejason0 <sunliyuan200402@outlook.com>
2020-03-08 19:09:35 +0800

    Hmm

--- src/main/java/ml/lakejason0/Multiverse/Multiverse.java
+++ src/main/java/ml/lakejason0/Multiverse/Multiverse.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package ml.lakejason0.Multiverse;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Multiverse implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
